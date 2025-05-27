d6b88c5ade651ac215a60004c1b3ccd3734a7ba5
Damian <damian.balaz@gmail.com>
2025-05-27 22:07:23 +0200

    Changed the size of the rendered particles and springs

--- renderer.py
+++ renderer.py
@@ -8,10 +8,10 @@ class Renderer:
     def draw(self, particles: list, springs: list):
         # draw springs
         for s in springs:
-            pygame.draw.line(self.screen, (200, 200, 200), s.p1.pos, s.p2.pos, 2)
+            pygame.draw.line(self.screen, (200, 200, 200), s.p1.pos, s.p2.pos, 5)
         # draw particles
         for p in particles:
             color = (255, 0, 0) if p.fixed else (0, 0, 255)
-            pygame.draw.circle(self.screen, color, (int(p.pos.x), int(p.pos.y)), 5)
+            pygame.draw.circle(self.screen, color, (int(p.pos.x), int(p.pos.y)), 10)
 
 
